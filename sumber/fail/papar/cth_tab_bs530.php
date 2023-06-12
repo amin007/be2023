d94bb63f46b56d58a21b8f8f7ed5cc5d9a68ac55
Amin Dell Precision 3510 <helmi007@yahoo.com>
2023-06-12 16:59:18 +0800

    contoh buat tab untuk bootstrap 5.3.0

---
+++ sumber/fail/papar/cth_tab_bs530.php
@@ -0,0 +1,47 @@
+<!-- mula kotak
+=============================================================================================== -->
+<div class="kotakAtas">
+<div class="kotakTengah">
+<!-- mula borang
+=============================================================================================== -->
+<ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
+<li class="nav-item" role="presentation">
+	<button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+	data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="profile-tab" data-bs-toggle="tab"
+	data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane"
+	aria-selected="false" tabindex="-1">Profile</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="contact-tab" data-bs-toggle="tab"
+	data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane"
+	aria-selected="false" tabindex="-1">Contact</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="disabled-tab" data-bs-toggle="tab"
+	data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane"
+	aria-selected="false" disabled="" tabindex="-1">Disabled</button>
+</li>
+</ul>
+<div class="tab-content" id="myTabContent">
+	<div class="tab-pane fade active show" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+		<p>This is some placeholder content the <strong>Home tab's</strong> associated content.</p>
+	</div><!-- class="tab-pane fade" -->
+	<div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+		<p>This is some placeholder content the <strong>Profile tab's</strong> associated content.</p>
+	</div><!-- class="tab-pane fade" -->
+	<div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+		<p>This is some placeholder content the <strong>Contact tab's</strong> associated content.</p>
+	</div><!-- class="tab-pane fade" -->
+	<div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">
+		<p>This is some placeholder content the <strong>Disabled tab's</strong> associated content.</p>
+	</div><!-- class="tab-pane fade" -->
+</div><!-- / class="tab-content" ->
+<!-- tamat borang
+=============================================================================================== -->
+</div><!-- / class="kotakTengah" -->
+</div><!-- / class="kotakAtas" -->
+<!-- tamat kotak
+=============================================================================================== -->
