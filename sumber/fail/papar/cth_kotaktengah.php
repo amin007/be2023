0038a36f3fd857a58dee06a75d148c9cd1c8ce5b
Amin Dell Precision 3510 <helmi007@yahoo.com>
2023-06-12 14:30:46 +0800

    simpan kod untuk buat tab dalam bootstrap

---
+++ sumber/fail/papar/cth_kotaktengah.php
@@ -0,0 +1,103 @@
+<div class="kotakAtas">
+<div class="kotakTengah">
+	<ul class="nav nav-tabs well" id="myTab" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" id="anjung-tab" data-toggle="tab" href="#anjung" role="tab" aria-controls="anjung" aria-selected="true">Anjung</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="warna-tab" data-toggle="tab" href="#warna" role="tab" aria-controls="warna" aria-selected="false">Warna</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="false">Login</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="projek-tab" data-toggle="tab" href="#projek" role="tab" aria-controls="projek" aria-selected="false">Projek</a>
+	</li>
+	</ul>
+	<div class="tab-content" id="myTabContent">
+		<div class="tab-pane fade show active" id="anjung" role="tabpanel" aria-labelledby="anjung-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="form-group">
+		<label for="a" class="bg-white">
+			<a href="v3/">Selamat datang ke halaman kami</a>
+		</label><br>
+		<label class="well">Sempena hari raya yang mulia ini,</label><br>
+		<label class="well"> saya ingin mengucapkan</label><br>
+		<label class="well">Selamat tiba-tiba hari raya Aidilfitri,</label><br>
+		<label class="well">Maaf zahir dan batin</label><br>
+			</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="warna" role="tabpanel" aria-labelledby="warna-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br>
+	<div class="p-3 mb-2 bg-transparent text-dark shadow-lg rounded">
+		<a href="v3/rujukan">Rujukan Utama</a>
+	</div>
+	<div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+	<div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+	<div class="p-3 mb-2 bg-success text-white">.bg-success</div>
+	<div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+	<div class="p-3 mb-2 bg-warning text-dark">.bg-warning</div>
+	<div class="p-3 mb-2 bg-info text-white">.bg-info</div>
+	<div class="p-3 mb-2 bg-light text-dark">.bg-light</div>
+	<div class="p-3 mb-2 bg-dark text-white">.bg-dark</div>
+	<div class="p-3 mb-2 bg-white text-dark">.bg-white</div>
+	<div class="p-3 mb-2 bg-transparent text-dark">.bg-transparent</div>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="p-3 mb-2 bg-transparent text-dark">
+		<i class="fa fa-bicycle" style="font-size:50px"></i>
+	</div><!-- / class="p-3 mb-2 bg-transparent text-dark" -->
+	<div class="form-group">
+		<div class="btn btn-secondary btn-block text-white">Login dari Facebook <i class="fab fa-facebook"></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Github <i class="fab fa-github"></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Google <i class='fab fa-google'></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Twitter <i class="fab fa-twitter-square"></i></div>
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label class="border border-dark btn-block">Atau/Or</label>
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label for="exampleInputEmail1">Email</label>
+		<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Email">
+		<small id="emailHelp" class="form-text text-muted">Kita berjanji tidak akan berkongsi kasih<br>
+		eh salah berkongsi email dengan orang lain.</small>
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Rahsia Antara Kita</label>
+		<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+	</div>
+	<div class="form-group form-check">
+		<input type="checkbox" class="form-check-input" id="exampleCheck1">
+		<label class="form-check-label" for="exampleCheck1">Kenang daku dalam doamu</label>
+	</div>
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary btn-block" value="Hantar">
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<a class="btn btn-dark btn-block text-white" href="https://www.w3schools.com/icons/">
+		Rujukan Icon <i class="fas fa-headphones"></i></a>
+	</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="projek" role="tabpanel" aria-labelledby="projek-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="form-group">
+		<div class="bg-secondary text-white"><label class="kotakLabel" for="contohNama">
+			Belum putuskan lagi hendak letak apa di sini
+		<label></div>
+	</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+	</div><!-- / class="tab-content" id="myTabContent" -->
+</div><!-- / class="kotakTengah" -->
+</div><!-- / class="kotakAtas" -->
---
+++ sumber/fail/papar/cth_tab_loop.php
@@ -0,0 +1,33 @@
+<div class="kotakAtas">
+<div class="kotakTengah">
+	<ul class="nav nav-tabs well" id="myTab" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" id="anjung-tab" data-toggle="tab" href="#anjung" role="tab" aria-controls="anjung" aria-selected="true">Anjung</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="warna-tab" data-toggle="tab" href="#warna" role="tab" aria-controls="warna" aria-selected="false">Warna</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="false">Login</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="projek-tab" data-toggle="tab" href="#projek" role="tab" aria-controls="projek" aria-selected="false">Projek</a>
+	</li>
+	</ul>
+	<div class="tab-content" id="myTabContent">
+		<div class="tab-pane fade show active" id="anjung" role="tabpanel" aria-labelledby="anjung-tab">
+			<p>anjung bla bla bla</p>
+		</div><!-- / class="tab-pane fade" -->
+		</div>
+		<div class="tab-pane fade" id="warna" role="tabpanel" aria-labelledby="warna-tab">
+			<p>warna bla bla bla</p>
+		</div><!-- / class="tab-pane fade" -->
+		<div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+			<p>login bla bla bla</p>
+		</div><!-- / class="tab-pane fade" -->
+		<div class="tab-pane fade" id="projek" role="tabpanel" aria-labelledby="projek-tab">
+			<p>projek bla bla bla</p>
+		</div><!-- / class="tab-pane fade" -->
+	</div><!-- / class="tab-content" id="myTabContent" -->
+</div><!-- / class="kotakTengah" -->
+</div><!-- / class="kotakAtas" -->
