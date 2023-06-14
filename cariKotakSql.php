7670a11ac8949202bee6c7897b17e6761454aece
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-06-14 12:23:12 +0800

    dari $_POST masuk dalam sql terus

---
+++ cariKotakSql.php
@@ -0,0 +1,48 @@
+<?php
+###################################################################################################
+//require '/sumber/fail/php/***.php';
+require './tatarajah.php';
+require './sumber/fail/php/fungsi_global.php';
+//require '/sumber/fail/data/***.php';
+//require './sumber/fail/data/dataSql.php';
+//require './sumber/fail/csv/***.php';
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+$pilih = 'jadual01';
+list($urlcss,$urljs) = linkCssJs(1);// nilai default = 1
+diatasV01($pilih, $urlcss);
+binaButang(['jadual01','jadual02']);
+$class = '"table table-striped table-bordered"';
+#--------------------------------------------------------------------------------------------------
+print <<<END
+<!-- mula borang
+=============================================================================================== -->
+<form method="POST" action="paparSql04.php" class="form-horizontal">
+	<div class="p-3 mb-2 bg-transparent text-dark">
+		<i class="fa fa-bicycle" style="font-size:50px"></i>
+	</div><!-- / class="p-3 mb-2 bg-transparent text-dark" -->
+	<div class="form-group">
+		<label class="border border-dark btn-block">Carian Sql</label>
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label for="inputMsic">Arahan Sql</label>
+		<textarea rows="1" cols="20" name="sql" class="form-control"></textarea>
+	</div>
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary btn-block" value="Cari Data">
+	</div><!-- / class="form-group" -->
+</form>
+<!-- tamat borang
+=============================================================================================== -->
+END;
+	# tamat print <<<END
+#--------------------------------------------------------------------------------------------------
+dibawah($pilih,$urljs);
+echo "<script>\n";
+jqueryExtendA();
+jqueryExtendB();
+jqueryExtendC();
+gradeTable002(null);
+echo "\n</script>\n</body>\n</html>";//*/
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
