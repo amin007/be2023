72e23df5ac956616843779dc5818cd69a9ac2a59
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-06-17 12:57:11 +0800

    tambah css khas untuk table kecil

--- sumber/fail/php/fungsi_global.php
+++ sumber/fail/php/fungsi_global.php
@@ -908,7 +908,7 @@ if ( ! function_exists('diatasV02')):
 <meta name="author" content="">
 <title>$title</title>
 $linkCss
-<style>
+<style type="text/css">
 .kotakAtas
 {
 	text-align: center;
@@ -919,6 +919,27 @@ $linkCss
 {
 	text-align: center;
 	display: inline-block;
+}
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
 }
 </style>
 </head>
