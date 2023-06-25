779394f2016ba2c4127121c43292a39dc6198471
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-06-25 08:08:10 +0800

    masukkan kaedah tab dalam carian

--- paparSqlMsicID.php
+++ paparSqlMsicID.php
@@ -50,17 +50,47 @@ $data = dbMysqli01(DB_HOST,DB_NAME,DB_USER,DB_PASS,$sql);
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
 $pilih = null;
-list($urlcss,$urljs) = linkCssJs();
-$class = '"table table-striped table-bordered"';
+list($urlcss,$urljs) = linkCssJs(2);
+$class = '"table table-sm table-striped table-bordered"';
 diatas($pilih, $urlcss);
 #--------------------------------------------------------------------------------------------------
 binaButang(null);
+#--------------------------------------------------------------------------------------------------
+echo '<ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
+<li class="nav-item" role="presentation">
+<li class="nav-item" role="presentation">
+	<button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+	data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane"
+	aria-selected="true">Carian</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="belanja-tab" data-bs-toggle="tab"
+	data-bs-target="#belanja-tab-pane" type="button" role="tab" aria-controls="belanja-tab-pane"
+	aria-selected="false" tabindex="-1">belanja</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="gaji-tab" data-bs-toggle="tab"
+	data-bs-target="#gaji-tab-pane" type="button" role="tab" aria-controls="gaji-tab-pane"
+	aria-selected="false" tabindex="-1">gaji</button>
+</li>
+<li class="nav-item" role="presentation">
+	<button class="nav-link" id="disabled-tab" data-bs-toggle="tab"
+	data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane"
+	aria-selected="false" disabled="" tabindex="-1">harta</button>
+</li>
+</ul>
+<div class="tab-content" id="myTabContent">
+';
+#--------------------------------------------------------------------------------------------------
+echo '<div class="tab-pane fade active show" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">';
 #--------------------------------------------------------------------------------------------------
 foreach($data as $myJadualDaa => $rowDaa):
 	$table = paparSemuaData($rowDaa,$myJadualDaa);
 	echo "<h2>$myJadualDaa</h2>";
 	echo "\r<table id=\"allTable\" class=$class>$table</table>\r<hr>";
 endforeach;
+#--------------------------------------------------------------------------------------------------
+echo '</div><!-- class="tab-pane fade" id="home-tab-pane" -->';
 #--------------------------------------------------------------------------------------------------
 //$data[$kp337[9]]
 //semakPembolehubah($data[$kp337[9]],'data belanja',0);
@@ -71,11 +101,26 @@ foreach($data as $myJadualV02 => $rowV02):
 if($myJadualV02 == $kp337[9]):
 	//$table = paparSatuJe($rowV02,$myJadualV02);
 	$table = paparSatuDataAnggar($rowV02,$myJadualV02,$jumBelanja,$peratus);
+	#----------------------------------------------------------------------------------------------
+	echo '<div class="tab-pane fade" id="belanja-tab-pane" role="tabpanel" aria-labelledby="belanja-tab" tabindex="0">';
+	#----------------------------------------------------------------------------------------------
 	echo "<h2>$myJadualV02</h2>";
 	echo "\r<table id=\"allTable\" class=$class>$table</table>\r<hr>";
+	#----------------------------------------------------------------------------------------------
+	echo '</div><!-- class="tab-pane fade" id="belanja-tab-pane" -->';
+	#----------------------------------------------------------------------------------------------
 endif;
 endforeach;//*/
 #--------------------------------------------------------------------------------------------------
+echo '<div class="tab-pane fade" id="gaji-tab-pane" role="tabpanel" aria-labelledby="gaji-tab" tabindex="0">';
+echo '<p>untuk gaji</p>';
+echo '</div><!-- class="tab-pane fade" id="gaji-tab-pane" -->';
+#--------------------------------------------------------------------------------------------------
+echo '<div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">';
+echo '<p>This is some placeholder content the <strong>harta</strong> associated content.</p>';
+echo '</div><!-- class="tab-pane fade" id="disabled-tab-pane" -->';
+#--------------------------------------------------------------------------------------------------
+echo '</div><!-- / class="tab-content" ->';
 #--------------------------------------------------------------------------------------------------
 dibawah($pilih,$urljs);
 echo "<script>\n";
