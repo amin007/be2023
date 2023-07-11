31e6172ecd8b5e7aac44cec29f36b43d52e840d9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-07-11 13:10:00 +0800

    ubahsuai paparan data

--- paparJadualMenegak.php
+++ paparJadualMenegak.php
@@ -29,13 +29,13 @@ $id = bersih($_POST['noSiri']);//'000002791307';
 //$peratus = bersih($_POST['peratusan']);
 #--------------------------------------------------------------------------------------------------
 //$sql[] = '';
-$sql['dataBarcode'] = sqlCariBintangDaa($myJadual[0],'barcode',$id);
-$sql['dataAes'] = sqlCariBintangDaa($myJadual[1],'Serial No',$id);
-$sql['data5P'] = sqlCariBintangDaa($myJadual[2],'NO_SIRI',$id);
-$sql['dataSsmHarta'] = sqlCariBintangDaa($myJadual[3],'ESTABLISHMENT_ID',$id);
-$sql['dataUntungRugi'] = sqlCariBintangDaa($myJadual[4],'ESTABLISHMENT_ID',$id);
-$sql['dataKwsp'] = sqlCariBintangDaa($myJadual[5],'ESTABLISHMENT_ID',$id);
-$sql['dataMKO'] = sqlMkoDaa($myJadual[0],$id);
+$sql['barcode'] = sqlCariBintangDaa($myJadual[0],'barcode',$id);
+$sql['aes'] = sqlCariBintangDaa($myJadual[1],'Serial No',$id);
+$sql['limap'] = sqlCariBintangDaa($myJadual[2],'NO_SIRI',$id);
+$sql['ssmharta'] = sqlCariBintangDaa($myJadual[3],'ESTABLISHMENT_ID',$id);
+$sql['untungrugi'] = sqlCariBintangDaa($myJadual[4],'ESTABLISHMENT_ID',$id);
+$sql['kwsp'] = sqlCariBintangDaa($myJadual[5],'ESTABLISHMENT_ID',$id);
+$sql['mko'] = sqlMkoDaa($myJadual[0],$id);
 #--------------------------------------------------------------------------------------------------
 //echo '<hr>semakPembolehubah<hr>';
 //semakPembolehubah($myJadual,'myJadual',0);
@@ -48,12 +48,82 @@ $data = dbMysqli00(DB_HOST,DB_NAME,DB_USER,DB_PASS,$sql);
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
 $pilih = null;
-list($urlcss,$urljs) = linkCssJs();
+list($urlcss,$urljs) = linkCssJs(2);
 $class = '"table table-striped table-bordered"';
+//$class = '"table table-sm table-striped table-bordered"';
+$class02 = '"excel"';
 diatas($pilih, $urlcss);
 #--------------------------------------------------------------------------------------------------
 binaButang(null);
-echo "<!-- Senarai Jadual
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+# untuk bina tab berasasan $data
+#--------------------------------------------------------------------------------------------------
+echo "\n<!-- ================================================================================ -->";
+#--------------------------------------------------------------------------------------------------
+echo "\n" . '<ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
+<li class="nav-item" role="presentation">
+	<button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+	data-bs-target="#home-tab-pane" type="button" role="tab"
+	aria-controls="home-tab-pane" aria-selected="true">Carian</button>
+</li>';
+#--------------------------------------------------------------------------------------------------
+foreach($data as $namaTabLi => $barisDaa):
+$senaraiJadualJumpa[] = $namaTabLi;
+echo "\n" . '<li class="nav-item" role="presentation">
+	<button class="nav-link" id="' . $namaTabLi . '-tab" data-bs-toggle="tab"
+	data-bs-target="#' . $namaTabLi . '-tab-pane" type="button" role="tab"
+	aria-controls="' . $namaTabLi . '-tab-pane"
+	aria-selected="false" tabindex="-1">' . $namaTabLi . '</button>
+</li>';
+endforeach;
+#--------------------------------------------------------------------------------------------------
+echo "\n" . '<li class="nav-item" role="presentation">
+	<button class="nav-link" id="disabled-tab" data-bs-toggle="tab"
+	data-bs-target="#disabled-tab-pane" type="button" role="tab"
+	aria-controls="disabled-tab-pane"
+	aria-selected="false" disabled="" tabindex="-1">disabled</button>
+</li>';
+echo "\n" . '</ul>
+<div class="tab-content" id="myTabContent">';
+#--------------------------------------------------------------------------------------------------
+echo "\n<!-- ================================================================================ -->";
+#--------------------------------------------------------------------------------------------------
+echo "\n" . '<div class="tab-pane fade active show" id="home-tab-pane" role="tabpanel"'
+. ' aria-labelledby="home-tab" tabindex="0">';
+//semakPembolehubah($_POST['noSiri'],'_POST[noSiri]',0);
+semakPembolehubah($senaraiJadualJumpa,'dataYangJumpa',0);
+echo "\n" . '</div><!-- class="tab-pane fade" id="home-tab-pane" -->';
+#--------------------------------------------------------------------------------------------------
+echo "\n<!-- ================================================================================ -->";
+#--------------------------------------------------------------------------------------------------
+foreach($data as $myJadualV02 => $rowV02):
+	$table = paparSatuJe($rowV02,$myJadualV02);
+	#------------------------------------------------------------------------------------------
+	echo "\n\t" . '<div class="tab-pane fade" id="' . $myJadualV02 . '-tab-pane"'
+	. ' role="tabpanel" aria-labelledby="' . $myJadualV02 . '-tab" tabindex="0">'
+	. "\n\t<h2>$myJadualV02</h2>"
+	. "\n\t<table class=$class02>$table</table>\n\t"
+	. "\n" . '</div><!-- class="tab-pane fade" id="' . $myJadualV02 . '-tab-pane" -->'
+	. "\n<!-- ========================================================================== -->";
+	#------------------------------------------------------------------------------------------
+endforeach;
+#--------------------------------------------------------------------------------------------------
+echo "\n\t";
+echo '<div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel"'
+. ' aria-labelledby="disabled-tab" tabindex="0">';
+echo "\n\t" . '<p>This is some placeholder content the <strong>disabled</strong> associated'
+. ' content.</p>';
+echo "\n" . '</div><!-- class="tab-pane fade" id="disabled-tab-pane" -->';
+#--------------------------------------------------------------------------------------------------
+echo "\n" . '</div><!-- / class="tab-content" -->';
+#--------------------------------------------------------------------------------------------------
+echo "\n<!-- ================================================================================ -->";
+//*/
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+echo "\n<hr><!-- Senarai Jadual
 ==============================================================================================="
 . " -->";
 #--------------------------------------------------------------------------------------------------
@@ -67,7 +137,9 @@ echo "<!-- Senarai Jadual
 	endif;
 	echo "\n</tr></table>\n";
 	#----------------------------------------------------------------------------------------------
-	endforeach;
+	endforeach;//*/
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
 #--------------------------------------------------------------------------------------------------
 dibawah($pilih,$urljs);
 echo "<script>\n";
