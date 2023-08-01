26cecf3f47e0a394211646f97924d9bd6b7e8272
Amin Dell Precision 3510 <helmi007@yahoo.com>
2023-08-01 22:41:11 +0800

    bawa 2 nilai dari sqlUpdateKawalan() dan masukkan dalam dbUpdate01()

--- updateKawalan.php
+++ updateKawalan.php
@@ -24,14 +24,14 @@ $respon = huruf('BESAR',$respon);
 $nota = huruf('BESAR',$nota);
 #--------------------------------------------------------------------------------------------------
 # setkan arahan sql sahaja
-$sql['updateKawalan'] = sqlUpdateKawalan($myJadual[0],$tarikh,$id,$respon,$nota);
+list($sql[$myJadual[0]],$dataKhas) = sqlUpdateKawalan($myJadual[0],$tarikh,$id,$respon,$nota);
 #--------------------------------------------------------------------------------------------------
 echo '<hr>semakPembolehubah<hr>';
 semakPembolehubah($_POST,'_POST',0);
 semakPembolehubah($myJadual,'myJadual',0);
 semakPembolehubah($sql,'sql',0);//*/
 #--------------------------------------------------------------------------------------------------
-//$data = dbMysqli01(DB_HOST,DB_NAME,DB_USER,DB_PASS,$sql);
+//$data = dbUpdate01(DB_HOST,DB_NAME,DB_USER,DB_PASS,$sql,$dataKhas);
 #--------------------------------------------------------------------------------------------------
 //semakPembolehubah($data,'data',0);
 #--------------------------------------------------------------------------------------------------
