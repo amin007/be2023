63c1e6ca0613d13351e488202d2c5dbbac312260
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-08-01 16:55:45 +0800

    buat update di database

---
+++ updateKawalan.php
@@ -0,0 +1,38 @@
+<?php
+###################################################################################################
+//require '/sumber/fail/php/***.php';
+require './tatarajah.php';
+require './sumber/fail/php/fungsi_global.php';
+//require '/sumber/fail/data/***.php';
+require './sumber/fail/data/dataSql.php';
+require './sumber/fail/data/dataSqlMysqli.php';
+//require './sumber/fail/csv/***.php';
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+//$fungsi = get_defined_functions();// internal or user
+//semakPembolehubah($fungsi,'fungsi',0);
+//echo '<pre>$fungsi=>';//print_r($fungsi['user']);//echo '<br></pre>' . "\n";
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+$namaFe = $tatarajahBatch[0];
+$jawatanFe = $tatarajahBatch[1];
+$namaPegawai = $tatarajahBatch[2];
+$jawatanPegawai = $tatarajahBatch[3];
+$tarikhBatch = bersih($_POST['tarikhBatch']);
+$noSiri = bersih($_POST['noSiri']);
+$catatanBatch = bersih($_POST['catatanBatch']);
+$tarikhBatchDaa = $tarikhBatch;
+$sql['updateKawalan'] = sqlUpdateKawalan($myJadual[0],$tarikhBatch,$noSiri,$catatanBatch);
+#--------------------------------------------------------------------------------------------------
+echo '<hr>semakPembolehubah<hr>';
+semakPembolehubah($_POST,'_POST',0);
+semakPembolehubah($myJadual,'myJadual',0);
+semakPembolehubah($sql,'sql',0);//*/
+#--------------------------------------------------------------------------------------------------
+//$data = dbMysqli01(DB_HOST,DB_NAME,DB_USER,DB_PASS,$sql);
+#--------------------------------------------------------------------------------------------------
+//semakPembolehubah($data,'data',0);
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+###################################################################################################
