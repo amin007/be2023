7f8d01a899091eb32ba1e061c905b85a155becd1
amin007 <helmi007@yahoo.com>
2023-08-19 21:51:11 +0800

    masukkan soalan 8 dan 9

---
+++ sumber/fail/data/dataKp328.php
@@ -0,0 +1,125 @@
+<?php
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+/*
+Pendapatan drpd jualan makanan & minuman	F080040
+Nilai jualan dagangan	F080007
+Royalti, hakcipta, pelesenan & yuran francais	F080014
+Pendapatan drpd perkhidmatan pengurusan	F080010
+Komisen & brokeraj yg diterima	F080011
+Pendapatan perkhidmatan yg diterima	F080022
+Hasil Sewa: Tanah	F080012
+Hasil Sewa:Bangunan tempat kediaman	F080034
+Hasil Sewa:Bangunan bukan tempat kediaman	F080035
+Hasil Sewa:Alat pengangkutan	F080039
+Hasil Sewa:Jentera & kelengkapan	F080061
+Hasil Sewa:Perabot & pemasangan	F080062
+Hasil Sewa:Sewa ke atas ruangan (cth : kios penyediaan makanan & minuman)	F080031
+Hasil Sewa: Lain-lain	F080013
+Hasil X Operasi:Subsidi	F080063
+Hasil X Operasi:Subsidi gaji & upah	F080064
+Hasil X Operasi:Subsidi produk	F080065
+Hasil X Operasi:Subsidi pengeluaran	F080066
+Hasil X Operasi:Tuntutan & pampasan yg diterima	F080067
+Hasil X Operasi:Pemulihan hutang lapuk	F080068
+Hasil X Operasi:Pendapatan drpd faedah	F080069
+Hasil X Operasi:Pendapatan drpd dividen	F080070
+Hasil X Operasi:Keuntungan drpd jualan/penilaian semula harta	F080071
+Hasil X Operasi:Keuntungan drpd pertukaran mata wang asing/aset kewangan	F080072
+Hasil X Operasi:Kiriman wang, hadiah atau geran yg diterima	F080073
+Hasil X Operasi:Lain-lain pendapatan bkn operasi (sila nyatakan)	F080074
+NOTA F080074	F080074a
+pendapatan drpd aktiviti e-sukan?	F080075
+Jika Ya, nyatakan pendapatan drpd aktiviti e-sukan ini	F080076
+Lain-lain pendapatan operasi (sila nyatakan)	F080016
+NOTA F080016	F080016a
+Jumlah pendapatan (8.1 hingga 8.10)	F080089
+Pindahan modal yg diterima	F080017
+JUMLAH HASIL BESAR (8.11 & 8.12)	F080099
+*/
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+/*
+Untuk penyediaan makanan & minuman	F090001
+Kos barangan yg dijual	F090009
+Nilai bekalan2 lain yg digunakan:Bahan pembungkusan	F090002
+Nilai bekalan2 lain yg digunakan:Pinggan mangkuk, barangan kaca, alas meja dsb.	F090112
+Nilai bekalan2 lain yg digunakan:Bahan pencuci	F090113
+Nilai bekalan2 lain yg digunakan:Bahan untuk pembaikan & penyelenggaraan	F090003
+Nilai bekalan2 lain yg digunakan:Alat tulis & bekalan 	F090005
+Nilai bekalan2 lain yg digunakan:Lain-lain	F090057
+Kos percetakan	F090058
+Air yg dibeli	F090006
+Tenaga elektrik yg dibeli	F090007
+Bahan pembakar, pelincir & gas	F090008
+Bayaran pembaikan & penyelenggaraan semasa yg dibuat oleh pihak lain bagi harta tetap pertubuhan ini	F090011
+Perbelanjaan penyelidikan & pembangunan	F090012
+r&d Dalaman%	F090013
+r&d Sumber luaran%	F090014
+Pembelian perkhidmatan pengangkutan	F090119
+Perbelanjaan perjalanan(claim)	F091002
+Perbelanjaan keraian	F091006
+Bayaran perakaunan, kesetiausahaan & audit	F091003
+Bayaran guaman	F091004
+Bayaran perkhidmatan profesional lain (cth. bayaran perundingan arkitek, kejuruteraan, juruukur dsb.)	F090080
+Bayaran pengurusan	F091005
+Komisen & bayaran agensi	F090109
+Bayaran pos (termasuk perkhidmatan kurier)	F091007
+Pengiklanan & promosi	F090063
+Bayaran bank	F091008
+Premium insurans kecuali insurans pampasan pekerja	F091009
+Bayaran pemprosesan data & lain-lain perkhidmatan yg berkaitan dgn teknologi maklumat	F090016
+Bayaran telekomunikasi (cth. telefon, internet dsb.)	F090017
+Bayaran sewa: Tanah	F090019
+Bayaran sewa: Bangunan	F090020
+Bayaran sewa: Jentera & kelengkapan	F090021
+Bayaran sewa: Kenderaan bermotor & jenis pengangkutan lain	F090022
+Bayaran sewa: Lain-lain	F090023
+Susut nilai/perlunasan semasa ke atas harta tetap	F090024
+Bayaran faedah	F090025
+Bayaran bagi perkhidmatan keselamatan	F090067
+Bayaran royalti kpd:Kerajaan/Badan Berkanun (sila nyatakan jenis royalti)	F090026
+nota F090026	F090026a
+Bayaran royalti kpd:NGO/tajaan korporat (sila nyatakan jenis royalti)	F090027
+nota F090027	F090027a
+Taksiran (ke atas tanah & bangunan) & cukai tanah	F090030
+Cukai jalan	F090031
+Bayaran pendaftaran perniagaan, lesen memandu dsb.	F090032
+Cukai perkhidmatan atau cukai jualan	F091010
+Kerugian drpd pertukaran wang asing/aset kewangan	F091011
+Kerugian drpd jualan/penilaian semula harta	F091012
+Hutang lapuk dihapuskan	F091013
+Pindahan semasa seperti pindahan wang, hadiah, derma, denda, dsb.	F091014
+Lain-lain perbelanjaan bkn operasi (sila nyatakan)	F091015
+nota F091015	F091015a
+perbelanjaan dlm aktiviti e-sukan?	F091016
+Jika Ya, jumlah perbelanjaan dlm aktiviti e-sukan ini	F091017
+Lain-lain perbelanjaan operasi (sila nyatakan)	F090035
+nota F090035	F090035a
+Gaji & upah dibayar	F090036
+Bayaran pampasan, persaraan/pemberhentian kpd pekerja	F090037
+Bayaran kpd pekerja:Rawatan perubatan percuma	F090038
+Bayaran manfaat kpd pekerja:Lain-lain seperti makanan percuma, tempat tinggal percuma dsb.	F090039
+Caruman:Kumpulan Wang Simpanan Pekerja (KWSP?/EPF)	F090040
+Caruman:Kumpulan Wang Simpanan Lain	F090041
+Caruman:Pertubuhan Keselamatan Sosial (PERKESO/SOCSO)	F090042
+Caruman:Skim keselamatan sosial persendirian (cth. insurans pampasan pekerja dsb.)	F090043
+Caruman:Skim bayaran pampasan, persaraan/pemberhentian	F090044
+Bayaran kpd pengarah tidak bekerja kerana hadir mesyuarat Lembaga Pengarah	F090045
+Nilai pakaian percuma yg disediakan	F090046
+Kos latihan kpd pekerja	F090047
+Kos pengangkutan pekerja (pergi & balik dari tempat kerja)	F090048
+Bayaran levi pekerja	F090049
+Perbelanjaan pembayaran berasaskan saham kpd pekerja	F090050
+Kos pekerja lain (sila nyatakan)	F090051
+nota F090051	F090051a
+Bayaran kpd pertubuhan lain yg membekalkan pekerja	F090052
+Jumlah perbelanjaan (9.1 hingga 9.33)	F090089
+Pindahan modal yg dibuat	F090053
+Perbelanjaan pajakan kewangan	F090054
+Dividen dibayar	F090055
+Cukai langsung dibayar (cth: cukai syarikat & duti setem)	F090056
+JUMLAH BELANJA BESAR (9.34 hingga 9.38)	F090099
+*/
+#--------------------------------------------------------------------------------------------------
