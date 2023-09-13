1165c080b82711596f84e2e0559eb16b10f75903
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-09-13 15:22:29 +0800

    tambah medan KP100

---
+++ sumber/fail/data/dataKp100.php
@@ -0,0 +1,452 @@
+<?php
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+/*
+nota
+8.1-Pendapatan daripada aktiviti tanaman:
+(d) Nilai jualan barangan / bahan / produk berkaitan dengan aktiviti tanaman, yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. buah, sayur, baja)
+8.2-Pendapatan daripada aktiviti ternakan:
+(d) Nilai jualan barangan / bahan / produk berkaitan dengan aktiviti ternakan, yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. haiwan ternakan, makanan haiwan, mineral)
+8.3-Pendapatan daripada aktiviti perikanan:
+(d) Nilai jualan barangan / bahan / produk berkaitan dengan aktiviti perikanan, yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. ikan, makanan ikan, pukat)
+8.4-Pendapatan daripada aktiviti perhutanan:
+(c) Nilai jualan barangan / bahan / produk berkaitan aktiviti perhutanan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. balak, gergaji rantai, tukul balak)
+9.1-Perbelanjaan untuk aktiviti tanaman:
+(c) Kos barangan / bahan / produk berkaitan dengan aktiviti tanaman yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. buah, sayur)
+9.2-Perbelanjaan untuk aktiviti ternakan:
+(c) Kos barangan / bahan / produk berkaitan dengan aktiviti ternakan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. lembu, ayam)
+9.3-Perbelanjaan untuk aktiviti perikanan:
+(c) Kos barangan / bahan / produk berkaitan dengan aktiviti perikanan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. ikan)
+9.4-Perbelanjaan untuk aktiviti perhutanan:
+(b) Kos barangan / bahan / produk berkaitan dengan aktiviti perhutanan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya (cth. balak, gergaji rantai, tukul balak)
+*/
+#--------------------------------------------------------------------------------------------------
+PEROLEHAN / PENDAPATAN | TURNOVER / INCOME
+TANAMAN / CROPS
+Jualan drpd tanaman dan hasil tanaman	F084001
+Pendapatan drpd kerja tanaman yang dibuat untuk pihak lain(cth. tanaman kontrak, menanam, membersih tanah)	F084002
+#Jawapan mesti sama dengan medan F164316
+Pendapatan drpd hasil tanaman yang diproses(cth. kerepek ubi, sayur jeruk, air buah)	F084003
+Nilai jualan(trading) xtvt tani(cth. buah, sayur, baja)	F084004
+Pendapatan lain yang berkaitan dengan tanaman(cth. khidmat nasihat)	F084005
+TERNAKAN / LIVESTOCK
+Jualan drpd hasil ternakan:(i)ternakan hidup	F084006
+#Jawapan mesti sama dengan medan F165306
+Jualan drpd hasil ternakan:(ii)hasil ternakan(cth. telur, susu)	F084007
+#Jawapan mesti sama dengan medan F166306
+Pendapatan drpd kerja ternakan yang dibuat untuk pihak lain(cth. ternakan kontrak, pawah)	F084008
+Pendapatan drpd hasil ternakan yang diproses(cth. nugget, daging burger)	F084009
+Nilai jualan(trading) xtvt ternakan(cth. haiwan ternakan, makanan haiwan, mineral)	F084010
+Pendapatan lain yang berkaitan dengan ternakan(cth. baja drpd tinja, khidmat nasihat)	F084011
+PERIKANAN / FISHERIES
+Jualan drpd tangkapan ikan marin atau hasil drpd akuakultur	F084012
+#Jawapan mesti sama dengan medan F167316
+Pendapatan drpd kerja perikanan yang dibuat untuk pihak lain(cth. perikanan kontrak, menangkap ikan)	F084013
+Pendapatan drpd hasil perikanan yang diproses(cth. keropok ikan, bebola ikan, ikan kering, minyak gamat, baja dari ikan)	F084014
+Nilai jualan(trading) xtvt ikan(cth. ikan, makanan ikan, pukat)	F084015
+Pendapatan lain yang berkaitan dengan perikanan(cth. baiki bot / pukat, sewaan bilik beku, khidmat nasihat)	F084016
+PERHUTANAN & PEMBALAKAN / FORESTRY & LOGGING
+Jualan balak	F084017
+#Jawapan mesti sama dengan medan F168316
+Pendapatan drpd kerja perhutanan yang dibuat untuk pihak lain (cth. mengukur, menanda, menebang)	F084018
+Nilai jualan(trading) xtxt hutan(cth. balak, gergaji rantai, tukul balak)	F084019
+Pendapatan lain yang berkaitan dengan aktiviti perhutanan (cth. perkhidmatan profesional dan juruukur dsb.)	F084020
+Kawasan perhutanan dikontrakkan kpd kontraktor	F084021
+Nilai jualan(trading) selain tani di atas	F080020
+Pendapatan output lain	F080009
+Pendapatan drpd perkhidmatan profesional	F080010			(cth. perakaunan, pengurusan, kejuruteraan, guaman, penyelidikan dan pembangunan dsb.)
+Komisen dan brokeraj yang diterima	F080011
+Pendapatan drpd agro pelancongan	F080021
+Hasil Sewa:Tanah	F080012
+Hasil Sewa:Bangunan tempat kediaman	F080034
+Hasil Sewa:Bangunan bukan tempat kediaman	F080035
+Hasil Sewa:Alat pengangkutan	F080039
+Hasil Sewa:Jentera & kelengkapan	F080061
+Hasil Sewa:Perabot & pemasangan	F080062
+Hasil Sewa:Lain-lain	F080013
+Royalti, hakcipta, pelesenan & yuran francais	F080014
+Hasil X Operasi:Subsidi produk	F080065
+Hasil X Operasi:Subsidi pengeluaran	F080066
+Hasil X Operasi:Tuntutan & pampasan yg diterima	F080067
+Hasil X Operasi:Pemulihan hutang lapuk	F080068
+Hasil X Operasi:Pendapatan drpd faedah	F080069
+Hasil X Operasi:Pendapatan drpd dividen	F080070
+Hasil X Operasi:Keuntungan drpd jualan/penilaian semula harta	F080071
+Hasil X Operasi:Keuntungan drpd pertukaran mata wang asing/aset kewangan	F080072
+Hasil X Operasi:Kiriman wang, hadiah atau geran yg diterima	F080073
+Hasil X Operasi:Lain-lain pendapatan bukan operasi (sila nyatakan)	F080074
+nota F080074	F080074a
+Adakah pendapatan drpd aktiviti e-sukan?*	F080075
+Jika Ya, sila nyatakan jumlah pendapatan dlm aktiviti e-sukan ini?	F080076
+Lain2 pendapatan operasi (sila nyatakan)	F080016
+nota F080016	F080016a
+Jumlah pendapatan (8.1 hingga 8.14)	F080089
+Pindahan modal yang diterima	F080017
+JUMLAH HASIL BESAR (8.15 hingga 8.16)	F080099
+#--------------------------------------------------------------------------------------------------
+PERBELANJAAN
+TANAMAN / CROPS
+Kos bahan langsung yang digunakan dlm aktiviti tanaman	F094001
+# Jawapan mesti sama dengan medan F174116 + F174216
+Kos bahan langsung:(i) Matang	F094002
+Kos bahan langsung:(ii) Belum matang	F094003
+Kos biji / benih tanaman jangka pendek (cth. benih sayuran)	F094004
+Kos barangan(trading) xtvt tanaman(cth. buah, sayur)	F094005
+Jumlah bayaran ke atas kerja-kerja tanaman yang dibuat oleh pihak lain(subkontrak):	F094006
+(subkontrak):(i) Menanam dan menuai / memetik	F094007
+(subkontrak):(ii) Merumput dan kawalan lalang	F094008
+(subkontrak):(iii) Menebang dan membersih tanah	F094009
+(subkontrak):(iv) Merawat dan mengawal penyakit	F094010
+(subkontrak):(v) Menyelenggara infrastruktur (cth. parit, jalan, jambatan)	F094011
+(subkontrak):(vi) Menyelenggara tempat / peralatan tanaman (cth. rumah hijau)	F094012
+(subkontrak):(vii) Kerja tanaman lain (nyatakan):	F094013
+nota F094013	F094013A
+TERNAKAN / LIVESTOCK
+Kos bahan langsung yang digunakan dlm aktiviti ternakan	F094014
+# Jawapan mesti sama dengan medan F175116 + F175216 = 0RM
+Kos bahan langsung:(i) Matang	F094015
+Kos bahan langsung:(ii) Belum matang	F094016
+Kos anak ternakan bukan baka (cth.anak ayam / anak itik)	F094017
+Kos barangan(trading) xtvt ternakan(cth. lembu, ayam)	F094018
+Jumlah bayaran ke atas kerja-kerja ternakan yang dibuat oleh pihak lain(subkontrak):	F094019
+(subkontrak):(i) Menternak	F094020
+(subkontrak):(ii) Merawat dan mengawal penyakit	F094021
+(subkontrak):(iii) Menyelenggara infrastruktur (cth. parit, jalan, jambatan)	F094022
+(subkontrak):(iv) Menyelenggara tempat / peralatan ternakan (cth. kandang)	F094023
+(subkontrak):(v) Kerja ternakan lain (nyatakan):	F094024
+nota F094024	F094024A
+PERIKANAN / FISHERIES
+Kos bahan langsung yang digunakan dlm aktiviti marin/akuakultur	F094025
+# Jawapan mesti sama dengan medan F176116 + F176216
+Kos bahan langsung:(i) Matang	F094026
+Kos bahan langsung:(ii) Belum matang	F094027
+Kos anak ternakan bukan baka (cth. anak ikan/anak udang)	F094028
+Kos barangan(trading) xtvt perikanan(cth. ikan)	F094029
+Jumlah bayaran ke atas kerja-kerja perikanan yang dibuat oleh pihak lain(subkontrak):	F094030
+(subkontrak):(i) Menangkap atau menternak / mengasuh	F094031
+(subkontrak):(ii) Merawat dan mengawal penyakit	F094032
+(subkontrak):(iii) Menyelenggara infrastruktur (cth. parit, jalan, jambatan)	F094033
+(subkontrak):(iv) Menyelenggara tempat / peralatan perikanan (cth. kolam, jaring)	F094034
+(subkontrak):(v) Menyelenggara vesel perikanan, kapal, jentera dsb.	F094035
+(subkontrak):(vi) Kerja perikanan lain (nyatakan):	F094036
+nota F094036	F094036A
+PERHUTANAN & PEMBALAKAN / FORESTRY & LOGGING
+Kos bahan langsung yang digunakan dlm aktiviti perhutanan	F094037
+# Jawapan mesti sama dengan medan F177116 + F177216 + F178317
+Kos barangan(trading) xtvt perhutanan(cth. balak, gergaji rantai, tukul balak)	F094038
+Jumlah bayaran (nilai kontrak) ke atas kerja yang dibuat oleh pihak lain menggunakan bahan yang dibekal oleh pertubuhan ini	F094039
+(subkontrak):(i) Binaan jalan, penyelenggaraan dan penjagaan(termasuk ukur tanah)	F094040
+(subkontrak):(ii) Binaan dan penyelenggaraan bangunan tempat kediaman / bukan tempat kediaman	F094041
+(subkontrak):(iii) Kos penyelenggaraan	F094042
+(subkontrak):(iv) Kos bancian pokok (termasuk mengukur dan menanda pokok)	F094043
+(subkontrak):(v) Kos perancangan pembalakan	F094044
+(subkontrak):(vi) Kerja-kerja menebang dan menual	F094045
+(subkontrak):(vii) Kos pengangkutan balak (cth. caj pemunggahan/memuatkan/berakit dsb.)	F094046
+Bahan dan bekas pembungkus yang digunakan (tidak termasuk kos bahan langsung di Soalan 17A hingga 17E)	F090002
+# Jawapan mesti sama dengan medan F150102 + F150202
+Bahan yang digunakan bagi pembaikan dan penyelenggaraan	F090003
+Alat tulis dan bekalan pejabat	F090005
+Air yang dibeli	F090006
+Tenaga elektrik yang dibeli	F090007
+Bahan pembakar, pelincir dan gas	F090008
+Bayaran pembaikan dan penyelenggaraan semasa yang dibuat oleh pihak lain bagi harta tetap pertubuhan ini	F090011
+Perbelanjaan penyelidikan dan pembangunan	F090012
+% r&d : Sumber Dalaman/In-house	F090013
+% r&d : Sumber luaran/Outsource	F090014
+Bayaran pemprosesan data dan lain-lain perkhidmatan yang berkaitan dengan teknologi maklumat	F090016
+Bayaran telekomunikasi (cth. telefon, internet dsb.)	F090017
+Bayaran Sewa Tanah	F090019
+Bayaran Sewaan Operasi dll	F090090
+Susut nilai / perlunasan semasa ke atas harta tetap	F090024
+Bayaran faedah	F090025
+Bayaran royalti kpd:Kerajaan/Badan Berkanun(sila nyatakan jenis royalti)	F090026
+nota F090026	F090026A
+Bayaran royalti kpd:NGO/tajaan korporat(sila nyatakan jenis royalti)	F090027
+nota F090027	F090027A
+Cukai tidak langsung dan lesen kerajaan:	F090077
+Cukai eksport	F090029
+Taksiran (ke atas tanah dan bangunan) dan cukai tanah	F090030
+Cukai jalan	F090031
+Ses Penyelidikan/Reasearch cess	F090078
+Bayaran pendaftaran perniagaan, lesen memandu, duti setem dsb.	F090032
+Cukai perkhidmatan atau cukai jualan	F091010
+Gaji & upah dibayar	F090036
+Bayaran pampasan, persaraan / pemberhentian kpd pekerja	F090037
+Bayaran berbentuk manfaat:(i) Rawatan perubatan percuma	F090038
+Bayaran berbentuk manfaat:(ii) Lain-lain seperti makanan percuma, tempat tinggal percuma dsb.	F090039
+Caruman:Kumpulan Wang Simpanan Pekerja (KWSP/EPF)	F090040
+Caruman:Kumpulan wang simpanan lain	F090041
+Caruman:Pertubuhan Keselamatan Sosial (PERKESO/SOCSO)	F090042
+Caruman:Skim keselamatan sosial persendirian (cth. insurans pampasan pekerja dsb)	F090043
+Caruman:Skim bayaran pampasan, persaraan / pemberhentian	F090044
+Bayaran kpd pengarah tidak bekerja kerana kehadiran mereka dlm mesyuarat Lembaga Pengarah	F090045
+Nilai pakaian percuma yang disediakan	F090046
+Kos latihan kpd pekerja	F090047
+Kos pengangkutan pekerja (pergi dan balik dari tempat kerja)	F090048
+Bayaran levi pekerja	F090049
+Perbelanjaan pembayaran berasaskan saham kpd pekerja	F090050
+Kos pekerja lain (sila nyatakan)	F090051
+nota F090051	F090051A
+Bayaran kpd pertubuhan lain yang membekalkan pekerja	F090052
+Pengiklanan dan promosi	F090063
+Bayaran bagi perkhidmatan keselamatan	F090067
+Perbelanjaan ke atas agro pelancongan	F090068
+Bayaran premium	F090069
+Kos barangan(trading)	F090070
+Bayaran perkhidmatan profesional lain (cth. bayaran perundingan arkitek, kejuruteraan, juruukur dsb.)	F090080
+Komisen dan bayaran agensi	F090109
+Pengangkutan barang (pengangkutan keluar)	F091001
+Perbelanjaan perjalanan(claim/parkir)	F091002
+Bayaran perakaunan, kesetiausahaan dan audit	F091003
+Bayaran guaman	F091004
+Bayaran pengurusan	F091005
+Perbelanjaan keraian	F091006
+Bayaran pos/kurier	F091007
+Bayaran bank	F091008
+Premium insurans dibayar ke atas bangunan, jentera, alat pengangkutan dan barang	F091009
+Belanja X Operasi:Kerugian drpd pertukaran wang asing / aset kewangan	F091011
+Belanja X Operasi:Kerugian drpd jualan / penilaian semula harta	F091012
+Belanja X Operasi:Hutang lapuk dihapuskan	F091013
+Belanja X Operasi:Pindahan semasa seperti pindahan wang, hadiah, derma,denda, dsb.	F091014
+Belanja X Operasi:Lain-lain perbelanjaan bukan operasi (sila nyatakan)	F091015
+nota F091015	F091015A
+Adakah pertubuhan ini mempunyai perbelanjaan dlm aktiviti e-sukan?	F091016
+Jika Ya, sila nyatakan jumlah perbelanjaan dlm aktiviti e-sukan ini	F091017
+Perbelanjaan input lain	F090035
+Lain-lain perbelanjaan operasi	F090079
+Jumlah perbelanjaan (9.1 hingga 9.41)	F090089
+Pindahan modal yg dibuat	F090053
+Perbelanjaan pajakan kewangan	F090054
+Dividen berbayar	F090055
+Cukai langsung dibayar (cth. Cukai syarikat dan duti setem)	F090056
+JUMLAH BELANJA BESAR (9.35 hingga 9.39)	F090099
+#--------------------------------------------------------------------------------------------------
+13A-KELUASAN TANAMAN JANGKA PANJANG SEPERTI PADA 31 DISEMBER 2022
+Jenis Tanaman
+Type of Crops	Keluasan tanaman (Hektar)
+Planted area (Hectares)	Keluasan tanam semula pada 2022
+Replanted area during 2022
+
+Hektar / Hectares	Keluasan tanam baru pada 2022
+Newly planted area during 2022
+
+Hektar / Hectares	UNTUK KEGUNAAN PEJABAT SAHAJA
+
+Jenis Tanaman
+Keluasan tanaman semula(Hektar)
+Keluasan tanaman baru(Hektar)
+Matang
+Belum matang
+Kod produk
+
+
+Kelapa Sawit
+Oil Palm
+ F134001
+ F134101
+Jawapan mesti sama dengan medan F134201+F134301 = 0
+ F134201
+ F134301
+ F134401
+0126101001
+Getah
+Rubber
+ F134002
+ F134102
+Jawapan mesti sama dengan medan F134202+F134302 = 0
+ F134202
+ F134302
+ F134402
+0129101001
+Lain-lain (nyatakan):
+Others (specify):
+
+ F134403C
+ F134003
+ F134103
+Jawapan mesti sama dengan medan F134203+F134303 = 0
+ F134203
+ F134303
+ F134403
+
+ F134404D
+ F134004
+ F134104
+Jawapan mesti sama dengan medan F134204+F134304 = 0
+ F134204
+ F134304
+ F134404
+
+ F134405E
+ F134005
+ F134105
+Jawapan mesti sama dengan medan F134205+F134305 = 0
+ F134205
+ F134305
+ F134405
+
+JUMLAH
+TOTAL
+ F134006
+ F134106
+Jawapan mesti sama dengan medan F134206+F134306 = 0
+ F134206
+ F134306
+
+#--------------------------------------------------------------------------------------------------
+13B-STATUS HUTAN, SISTEM PEMBALAKAN DAN KELUASAN PEMBALAKAN
+13B.1
+Status hutan:
+Sila tanda [1] di kotak yang berkaitan (boleh tanda lebih dari satu kotak):
+Hutan Tanah Berimilik	Alienated land	F134501
+Hutan Tanah Kerajaan	Stateland Forest	F134502
+Hutan Simpanan Kekal	Permanent Reserved Forest	F134503
+Ladang Hutan Kekal	Permanent Forest Estate	F134504
+
+13B.2
+Sistem pembalakan:
+Sila tanda [1] di kotak yang berkaitan (boleh tanda lebih dari satu kotak):
+Jentolak	Bulldozer	F134505
+Helikopter	Helicopter	F134506
+Kabel	Cable	F134507
+Kuda-kuda	Sleigh logging	F134508
+Jentera penggali berasaskan pembalakan	Excavator-based logging (e.g. logfisher)	F134509
+Lain-lain (nyatakan):	Others (specify):	F134510
+	nota F134510	F134512
+13B.3
+Jumlah keluasan pembalakan(Hektar):	F134511
+#--------------------------------------------------------------------------------------------------
+Jenis Akuakultur			Bilangan stok induk / baka akuakultur	Bilangan stok akuakultur
+		Matang	Belum matang	Matang	Belum Matang	Kod produk
+#--------------------------------------------------------------------------------------------------
+Akuakultur - Laut / Aquaculture - marine
+Udang Putih
+White Shrimp
+F144001
+F144101
+F144201
+F144301
+F144401
+0321401002
+Kerapu
+Grouper
+F144002
+F144102
+F144202
+F144302
+F144402
+0321102003
+Lain-lain (nyatakan):
+Others (specify):
+
+F144403C
+F144003
+F144103
+F144203
+F144303
+F144403
+
+F144404D
+F144004
+F144104
+F144204
+F144304
+F144404
+
+F144405E
+F144005
+F144105
+F144205
+F144305
+F144405
+
+F144406F
+F144006
+F144106
+F144206
+F144306
+F144406
+
+F144407G
+F144007
+F144107
+F144207
+F144307
+F144407
+
+F144408H
+F144008
+F144108
+F144208
+F144308
+F144408
+
+Akuakultur - Air Tawar / Aquaculture - Freshwater
+Tilapia Merah
+Red Tilapia
+F144009
+F144109
+F144209
+F144309
+F144409
+0322102039
+Keli
+Walking catfish
+F144010
+F144110
+F144210
+F144310
+F144410
+0322102014
+Lain-lain (nyatakan):
+Others (specify):
+
+F144411K
+F144011
+F144111
+F144211
+F144311
+F144411
+
+F144412L
+F144012
+F144112
+F144212
+F144312
+F144412
+
+F144413M
+F144013
+F144113
+F144213
+F144313
+F144413
+
+F144414N
+F144014
+F144114
+F144214
+F144314
+F144414
+
+F144415O
+F144015
+F144115
+F144215
+F144315
+F144415
+
+JUMLAH
+TOTAL
+F144016
+F144116
+F144216
+F144316
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
