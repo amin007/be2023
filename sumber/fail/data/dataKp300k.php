b8c51ff2dd58934e966ed6b40784fae925b8fc69
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-09-18 15:10:40 +0800

    tambah kp 300k

---
+++ sumber/fail/data/dataKp300k.php
@@ -0,0 +1,193 @@
+<?php
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+Untuk Soalan 8.1 perlu diisi sama ada di 8.1A atau 8.1B berdasarkan aktiviti yang berkaitan dengan pertubuhan ini.
+For Question 8.1 it must be filled either 8.1A or 8.1B according to the activity that relates with this establishment.
+	Pendapatan daripada faedah / pembiayaan	F087701
+	Faedah dan pendapatan daripada pinjaman dan pembiayaan	F087704
+	Pajakan kewangan / sewa beli	F087705
+	Pemfaktoran	F087706
+	Pendapatan daripada faedah	F080069
+Pendapatan daripada premium:
+(i) Insurans / takaful hayat	F087708
+(ii) Insurans / takaful am:
+      (a) Marin, udara dan transit	F087709
+      (b) Kenderaan bermotor	F087710
+      (c) Kebakaran dan kerosakan harta lain	F087711
+      (d) Perubatan	F087712
+      (e) Pelbagai/Miscellaneous	F087713
+	Pendapatan daripada perkhidmatan pengurusan / fi / perundingan	F087714
+	Komisen dan brokeraj yang diterima	F087715
+	Nilai duti setem dikutip atas urusan jual beli saham	F087716
+	Pendapatan daripada dividen	F080070
+	Keuntungan daripada pertukaran mata wang asing / aset kewangan	F080072
+	Royalti, hakcipta, pelesenan dan yuran francais	F080014
+	Hasil Sewa Tanah	F080012
+	Hasil Sewa Bangunan tempat kediaman	F080034
+	Hasil Sewa Bangunan bukan tempat kediaman 	F080035
+	Hasil Sewa Alat pengangkutan	F080039
+	Hasil Sewa Jentera dan kelengkapan	F080061
+	Hasil Sewa Perabot dan pemasangan	F080062
+	Hasil Sewa Lain-lain(cth. lot kedai, ruang dsb.)	F080013
+	Jumlah Subsidi	F080063
+	Subsidi gaji & upah	F080064
+	Subsidi produk	F080065
+	Subsidi pengeluaran	F080066
+	Tuntutan & pampasan yg diterima	F080067
+	Pemulihan hutang lapuk	F080068
+	Keuntungan daripada jualan/penilaian semula harta	F080071
+	Keuntungan daripada pertukaran mata wang asing/aset kewangan	F080072
+	Kiriman wang, hadiah atau geran yg diterima	F080073
+	Lain-lain pendapatan bukan operasi (sila nyatakan)	F080074
+	nota Lain2 pendapatan bukan operasi	F080074a
+	Adakah pertubuhan ini mempunyai pendapatan drp aktiviti e-sukan?	F080075
+	Jika Ya, sila nyatakan jumlah pendapatan dalam aktiviti e-sukan ini	F080076
+	Lain-lain pendapatan operasi (sila nyatakan)	F080016
+	nota Lain2 pendapatan operasi	F080016a
+	Jumlah pendapatan (8.1 hingga 8.11) 	F080089
+	Pindahan modal yg diterima	F080017
+	JUMLAH HASIL BESAR (8.12 hingga 8.12)	F080099
+#--------------------------------------------------------------------------------------------------
+Bayaran pengurusan dan Sistem Depositori Berpusat	F097701
+Bayaran utk:a)Perbelanjaan pembiayaan dan faedah ke atas deposit	F097702
+Bayaran utk:b)Kadar faedah / keuntungan terapung	F097703
+Bayaran utk:c) Faedah / pembayaran lain	F097704
+Tuntutan bersih kena dibayar (faedah polisi bersih):
+i) Insurans / takaful hayat	F097705
+ii) Insurans / takaful am :
+a) Marin, udara dan transit	F097706
+b) Kenderaan bermotor	F097707
+c) Kebakaran & kerosakan harta lain	F097708
+d) Perubatan	F097709
+e) Pelbagai	F097710
+Premium insurans kecuali insurans pampasan pekerja	F091009
+Bayaran faedah	F090025
+Nilai bekalan2 yang lain digunakan:a) Bahan untuk pembaikan dan penyelenggaraan	F090003
+Nilai bekalan2 yang lain digunakan:b) Alat tulis dan bekalan pejabat	F090005
+Nilai bekalan2 yang lain digunakan:c) Lain-lain	F090057
+Kos percetakan	F090058
+Air yg dibeli	F090006
+Tenaga elektrik yg dibeli	F090007
+Bahan pembakar, pelincir dan gas	F090008
+Bayaran pembaikan & penyelenggaraan semasa yang dibuat oleh pihak lain bagi harta tetap pertubuhan ini	F090011
+Perbelanjaan penyelidikan dan pembangunan	F090012
+% r&d :a) Sumber Dalaman/In-house	F090013
+% r&d :b) Sumber luaran/Outsource	F090014
+Bayaran pemprosesan data dan lain-lain perkhidmatan yang berkaitan dengan teknologi maklumat	F090016
+Bayaran telekomunikasi (cth. telefon, internet dsb.)	F090017
+Bayaran bagi perkhidmatan keselamatan	F090067
+Pengiklanan dan promosi	F090063
+Bayaran perkhidmatan profesional lain (cth. bayaran perundingan arkitek,kejuruteraan, juruukur dsb.)	F090080
+Bayaran perakaunan, kesetiausahaan & audit	F091003
+Bayaran guaman	F091004
+Bayaran pengurusan	F091005
+Perbelanjaan keraian	F091006
+Bayaran pos/kurier	F091007
+Bayaran bank	F091008
+Komisen dan bayaran agensi	F090109
+Pembelian perkhidmatan pengangkutan	F090119
+Bayaran Sewa: Tanah	F090019
+Bayaran Sewa: Bangunan	F090020
+Bayaran Sewa: Jentera dan kelengkapan	F090021
+Bayaran Sewa: Kenderaan bermotor & jenis pengangkutan lain	F090022
+Bayaran Sewa: Lain-lain	F090023
+Susut nilai / pelunasan semasa ke atas harta tetap	F090024
+Bayaran royalti kpd: Kerajaan / Badan Berkanun (sila nyatakan jenis royalti)	F090026
+nota F090026	F090026a
+Bayaran royalti kpd: NGO / tajaan korporat (sila nyatakan jenis royalti)	F090027
+nota F090027	F090027a
+Taksiran (ke atas tanah dan bangunan) dan cukai tanah	F090030
+Cukai jalan	F090031
+Bayaran pendaftaran perniagaan, lesen memandu dsb.	F090032
+Cukai perkhidmatan atau cukai jualan	F091010
+Perbelanjaan perjalanan(claim/parkir)	F091002
+Belanja X Operasi:Kerugian daripada pertukaran wang asing/aset kewangan	F091011
+Belanja X Operasi:Kerugian daripada jualan/penilaian semula harta	F091012
+Belanja X Operasi:Hutang lapuk dihapuskan	F091013
+Belanja X Operasi:Pindahan semasa seperti pindahan wang, hadiah, derma, denda, dsb.	F091014
+Belanja X Operasi:Lain-lain perbelanjaan bukan operasi (sila nyatakan)	F091015
+nota F091015	F091015a
+Adakah perbelanjaan drpd aktiviti e-sukan?*	F091016
+Jika Ya, sila nyatakan jumlah perbelanjaan dalam aktiviti e-sukan ini?	F091017
+Lain-lain perbelanjaan operasi (sila nyatakan)	F090035
+nota F090035	F090035a
+Gaji & upah dibayar	F090036
+Bayaran pampasan, persaraan/pemberhentian kepada pekerja	F090037
+Bayaran manfaat:Rawatan perubatan percuma	F090038
+Bayaran manfaat:Lain-lain seperti makanan percuma, tempat tinggal percuma dsb.	F090039
+Caruman:Kumpulan Wang Simpanan Pekerja (KWSP/EPF)	F090040
+Caruman:Kumpulan wang simpanan lain	F090041
+Caruman:Pertubuhan Keselamatan Sosial (PERKESO/SOCSO)	F090042
+Caruman:Skim keselamatan sosial persendirian (cth: EIS/insurans pampasan pekerja dsb.)	F090043
+Caruman:Skim bayaran pampasan, persaraan/pemberhentian	F090044
+Bayaran kpd pengarah tidak bekerja kerana kehadiran mereka dlm mesyuarat Lembaga Pengarah	F090045
+Nilai pakaian percuma yg disediakan	F090046
+Kos latihan kepada pekerja	F090047
+Kos pengangkutan pekerja	F090048
+Bayaran levi pekerja	F090049
+Perbelanjaan pembayaran berasaskan saham kepada pekerja (termasuk saham & pilihan saham)	F090050
+Kos pekerja lain (sila nyatakan)	F090051
+nota F090051	F090051a
+Bayaran kepada pertubuhan lain yg membekalkan pekerja	F090052
+Jumlah perbelanjaan (9.1 hingga 9.35)	 F090089
+Pindahan modal yg dibuat	F090053
+Perbelanjaan pajakan kewangan	F090054
+Dividen dibayar	F090055
+Cukai langsung dibayar (cth. cukai syarikat & duti setem)	F090056
+JUMLAH BELANJA BESAR (9.36 hingga 9.40)	F090099
+#--------------------------------------------------------------------------------------------------
+If F010030 is 64925, 64921, 64929 or 64999 then
+VALUE OF OUTPUT [(F087701 - F090025) + F087714 + F087715 +
+F080013 + F080016 + F080034 + F080035 + F080039 + F080061 + F080062 +
+F041399 + (F090013 X F090012 / 100)] must be greater than
+COST OF INPUT (
+F097701 + F090003 + F090005 + F090057 + F090058 +
+F090006 + F090007 + F090008 + F090011 +
+F090016 + F090017 +
+F090020 + F090021 + F090022 + F090023 + F090027 +
+F090035 + F090045 + F090046 + F090047 + F090052 +
+F090063 + F090067 + F090080 +
+F090109 + F090119 +
+F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+#--------------------------------------------------------------------------------------------------
+kad kredit
+If F010030 is 64922 then
+VALUE OF OUTPUT [F087701 + F087714 + F087715 + F080013 + F080016 + F080034 + F080035 + F080039 + F080061 + F080062 + F080072 + F041399 + (F090013 X F090012 / 100)]
+must be greater than
+COST OF INPUT (F097701 + F090003 + F090005 + F090057 + F090058 + F090006 + F090007 + F090008 + F090011 + F090016 + F090017 + F090020 + F090021 + F090022 + F090023 + F090027 + F090035 + F090045 + F090046 + F090047 + F090052 + F090063 + F090067 + F090080 + F090109 + F090119 + F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+#--------------------------------------------------------------------------------------------------
+If F010030 is 66121, 66122, 66123, 66124 or 66125 then
+VALUE OF OUTPUT [F087714 + F087715 + F080013 + F080016 + F080034 + F080035 + F080039 + F080061 + F080062 + F041399 + (F090013 X F090012 / 100)] must be greater than
+COST OF INPUT (F097701 + F090003 + F090005 + F090057 + F090058 + F090006 + F090007 + F090008 + F090011 + F090016 + F090017 + F090020 + F090021 + F090022 + F090023 + F090027 + F090035 + F090045 + F090046 + F090047 + F090052 + F090063 + F090067 + F090080 + F090109 + F090119 + F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+#--------------------------------------------------------------------------------------------------
+If F010030 is 64303, 64304, 64309, 64923, 64924, 64993, 66111, 66113, 66119, 66191, 66192, 66302, 66303 or 66199 then VALUE OF OUTPUT [F087701 + F087714 + F087715 + F080013 + F080016 + F080034 + F080035 + F080039 + F080061 + F080062 + F080072 + F041399 + (F090013 X F090012 / 100)] must be greater than
+COST OF INPUT (F097701 + F090003 + F090005 + F090057 + F090058 + F090006 + F090007 + F090008 + F090011 + F090016 + F090017 + F090020 + F090021 + F090022 + F090023 + F090027 + F090035 + F090045 + F090046 + F090047 + F090052 + F090063 + F090067 + F090080 + F090109 + F090119 + F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+#--------------------------------------------------------------------------------------------------
+If F010030 is 64200, 64301, 64992, 65301 or 65302 then
+VALUE OF OUTPUT (F097701 + F090036 + F090038 + F090039 + F090037 + F090040 + F090041 + F090042 + F090043 + F090044 + F090024 + F090003 + F090005 + F090057 + F090058 + F090006 + F090007 + F090008 + F090011 + F090020 + F090021 + F090022 + F090023 + F090027 + F090045 + F090046 + F090047 + F090048 + F090035 + F090052 + F090016 + F090017 + F090063 + F090067 + F090080 + F090109 + F090119 + F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+must be greater than
+COST OF INPUT (F097701 + F090003 + F090005 + F090057 + F090058 + F090006 + F090007 + F090008 + F090011 + F090016 + F090017 + F090020 + F090021 + F090022 + F090023 + F090027 + F090035 + F090045 + F090046 + F090047 + F090052 + F090063 + F090067 + F090080 + F090109 + F090119 + F091002 + F091003 + F091004 + F091005 + F091006 + F091007 + F091008 + F091009)
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
