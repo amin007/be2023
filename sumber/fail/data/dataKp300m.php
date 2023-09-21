c6246e7a7353574f9d4a2fdc6d58583fa2f370a9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-09-21 09:06:59 +0800

    tambah data baru

---
+++ sumber/fail/data/dataKp300m.php
@@ -0,0 +1,240 @@
+<?php
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+No	Keterangan	No Medan	Nilai/ Value
+
+Untuk Soalan 8.1 perlu diisi sama ada di 8.1A, 8.1B, 8.1C, 8.1D, 8.1E, 8.1F, 8.1G atau 8.1H berdasarkan aktiviti yang berkaitan dengan pertubuhan
+For Question 8.1 it must be filled either 8.1A, 8.1B, 8.1C, 8.1D, 8.1E, 8.1F, 8.1G atau 8.1H according to the activity that relates with this establishment.
+
+KP331-Perkhidmatan Pakar Runding Pengurusan dan Penyelidikan Pasaran
+Pendapatan daripada perkhidmatan perundingan F087901
+Peratus(%)
+(a) Perkhidmatan pengurusan dan perancangan strategik, struktur organisasi dan kajian F087902
+(b) Perkhidmatan perundingan pengurusan kewangan	F087903
+(c) Perkhidmatan perundingan pengurusan sumber manusia	F087904
+(d) Perkhidmatan agensi pekerjaan	F087905
+(e) Perkhidmatan perundingan pengurusan operasi	F087906
+(f) Perkhidmatan perundingan pengurusan pemasaran	F087907
+(g) Perkhidmatan perundingan saintifik dan teknikal	F087908
+(h) Perkhidmatan penganalisa pasaran	F087909
+(i) Perkhidmatan perundingan logistik	F087910
+(j) Perkhidmatan perundingan alam sekitar	F087911
+(k) Lain-lain perkhidmatan perundingan pengurusan	F087912
+% JUMLAH / TOTAL
+KP392-Perkhidmatan Penyelidikan dan Pembangunan Saintifik, Ujian Teknikal dan Analisis
+Pendapatan daripada perkhidmatan ujian teknikal dan analisis	F087913
+Pendapatan daripada penyelidikan dan pembangunan eksperimen sains semula jadi dan kejuruteraan	F087914
+Peratus
+(a) Penyelidikan dan pembangunan sains semula jadi	F087915
+(b) Penyelidikan dan pembangunan kejuruteraan dan teknologi	F087916
+(c) Penyelidikan dan pembangunan sains perubatan	F087917
+(d) Penyelidikan dan pembangunan bioteknologi	F087918
+(e) Penyelidikan dan pembangunan sains pertanian	F087919
+(f) Penyelidikan dan pembangunan teknologi maklumat dan komunikasi	F087920
+(g) Penyelidikan dan pembangunan sains semula jadi lain dan kejuruteraan lain	F087921
+% JUMLAH / TOTAL
+Pendapatan daripada penyelidikan dan pembangunan eksperimen sains sosial dan kemanusiaan	F087922
+Peratus
+(a) Penyelidikan dan pembangunan sains sosial	F087923
+(b) Penyelidikan dan pembangunan kemanusiaan	F087924
+(c) Penyelidikan dan pembangunan sains sosial dan kemanusiaan lain	F087925
+% JUMLAH / TOTAL
+KP304-Perkhidmatan Arkitek, Kejuruteraan dan Juruukur
+Pendapatan daripada perkhidmatan perundingan	F087974
+Peratus
+(a) Perkhidmatan perundingan seni bina dan reka bentuk awalan	F087926
+(b) Perkhidmatan perancangan bandar	F087927
+(c) Perkhidmatan seni bina landskap	F087928
+(d) Perkhidmatan kejuruteraan bersepadu	F087929
+(e) Perkhidmatan pengurusan projek	F087930
+(f) Perkhidmatan perundingan kejuruteraan	F087931
+(g) Perkhidmatan reka bentuk kejuruteraan	F087932
+(h) Perkhidmatan geologi, geofizik dan yang berkaitan	F087933
+(i) Perkhidmatan mengukur permukaan dan lapisan permukaan bumi	F087934
+(j) Perkhidmatan pemetaan	F087935
+(k) Lain-lain perkhidmatan yang berkaitan	F087936
+% JUMLAH / TOTAL
+KP305-Perkhidmatan Perakaunan
+Pendapatan daripada perkhidmatan perakaunan & simpan kira, audit dan perundingan	F087937
+Peratus
+(a) Perkhidmatan Perakaunan & Simpan Kira	F087938
+(b) Perkhidmatan Audit	F087939
+(c) Perkhidmatan Perundingan cukai	F087940
+(d)	Lain-lain (sila nyatakan)(i)	F087941
+nota F087941	F087941a
+(d)	Lain-lain (sila nyatakan)(ii)	F087942
+nota F087942	F087942a
+(d)	Lain-lain (sila nyatakan)(iii)	F087943
+nota F087943	F087943a
+% JUMLAH / TOTAL
+KP306-Perkhidmatan Guaman
+Pendapatan daripada perkhidmatan guaman, harta intelek, kesetiausahaan syarikat, notari awam / Pesuruhjaya Sumpah dsb.	F087944
+Peratus
+(a) Perkhidmatan Guaman	F087945
+(a)(i) Perkhidmatan Guaman: Yuran Penyerahan	F087946
+(a)(ii) Perkhidmatan Guaman: Lain-lain	F087947
+(b) Perkhidmatan Harta Intelek	F087948
+(c) Perkhidmatan Kesetiausahaan Syarikat	F087949
+(d) Notari Awam / Pesuruhjaya Sumpah	F087950
+(e)(i) Lain-lain (sila nyatakan)	F087951
+nota F087951	F087951a
+(e)(ii) Lain-lain (sila nyatakan)	F087952
+nota F087952	F087952a
+(e)(iii) Lain-lain (sila nyatakan)	F087953
+nota F087953	F087953a
+% JUMLAH / TOTAL
+KP312-Perkhidmatan Pengiklanan
+Pendapatan daripada perkhidmatan pengiklanan	F087954
+Peratus
+(a) Perkhidmatan pengiklanan penuh	F087955
+(b) Perkhidmatan pemasaran dan pengeposan secara terus	F087956
+(c) Perkhidmatan pengiklanan lain (sila nyatakan)	F087957
+% JUMLAH / TOTAL
+KP336-Perkhidmatan Veterinar
+(a) Perkhidmatan profesional doktor / pakar penjagaan kesihatan	F087958
+(b) Perkhidmatan ujian makmal, X-Ray dsb.	F087959
+(c) Ubat-ubatan dan bekalan ubatan lain	F087960
+KP393-Perkhidmatan Profesional Lain
+Pendapatan daripada aktiviti ibu pejabat	F087961
+Pendapatan daripada aktiviti mereka bentuk khusus	F087962
+Peratus
+(a) Aktiviti hiasan dalaman	F087963
+(b) Perkhidmatan pereka grafik	F087964
+(c) Perkhidmatan rekaan fesyen	F087965
+(d) Lain-lain	F087966
+% JUMLAH / TOTAL
+Pendapatan daripada aktiviti fotografi	F087967
+Pendapatan drpd aktiviti profesional,saintifik dan teknikal lain	F087968
+Peratus
+(a) Aktiviti penterjemahan dan penafsiran	F087969
+(b) Aktiviti broker perniagaan	F087970
+(c) Perunding keselamatan	F087971
+(d) Aktiviti perunding selain daripada perunding arkitek, kejuruteraan dan pengurusan	F087972
+(e) Lain-lain	F087973
+% JUMLAH / TOTAL
+Nilai jualan (barang / bahan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya)	F080007
+Pendapatan daripada perkhidmatan pengurusan	F080010
+Komisen dan brokeraj yang diterima	F080011
+Royalti, hakcipta, pelesenan dan yuran francais	F080014
+Hasil Sewa Tanah	F080012
+Hasil Sewa Bangunan tempat kediaman	F080034
+Hasil Sewa Bangunan bukan tempat kediaman 	F080035
+Hasil Sewa Alat pengangkutan	F080039
+Hasil Sewa Jentera dan kelengkapan	F080061
+Hasil Sewa Perabot dan pemasangan	F080062
+Hasil Sewa Lain-lain(cth. lot kedai, ruang dsb.)	F080013
+Jumlah Subsidi	F080063
+Subsidi gaji & upah	F080064
+Subsidi produk	F080065
+Subsidi pengeluaran	F080066
+Tuntutan & pampasan yg diterima	F080067
+Pemulihan hutang lapuk	F080068
+Pendapatan dari faedah	F080069
+Pendapatan dari dividen	F080070
+Keuntungan daripada jualan/penilaian semula harta	F080071
+Keuntungan daripada pertukaran mata wang asing/aset kewangan	F080072
+Kiriman wang, hadiah atau geran yg diterima	F080073
+Lain-lain pendapatan bukan operasi (sila nyatakan)	F080074
+nota Lain2 pendapatan bukan operasi	F080074a
+Adakah pertubuhan ini mempunyai pendapatan drp aktiviti e-sukan?	F080075
+Jika Ya, sila nyatakan jumlah pendapatan dalam aktiviti e-sukan ini	F080076
+Lain-lain pendapatan operasi (sila nyatakan)	F080016
+nota Lain2 pendapatan operasi	F080016a
+Jumlah pendapatan (8.1 hingga 8.11) 	F080089
+Pindahan modal yg diterima	F080017
+JUMLAH HASIL BESAR (8.12 hingga 8.12)	F080099
+#--------------------------------------------------------------------------------------------------
+BELANJA
+Nilai bekalan2 lain yang digunakan:a) Bahan pembungkusan	F090002
+Nilai bekalan2 lain yang digunakan:b) Bahan untuk pembaikan dan penyelenggaraan	F090003
+Nilai bekalan2 lain yang digunakan:c) Alat tulis dan bekalan pejabat	F090005
+Nilai bekalan2 lain yang digunakan:d) Lain-lain	F090057
+Untuk Soalan 9.2 hanya Survei 336-Perkhidmatan Veterinar sahaja
+(a) Nilai Ubat-ubatan	F090114
+(b) Nilai Bekalan perubatan lain yang digunakan	F090115
+Untuk Soalan 9.3 hanya Survei 312-Perkhidmatan Pengiklanan sahaja
+Kos perkhidmatan pengiklanan(termasuk kos pengeluaran,bayaran media, kos bahan dsb)	F097901
+Kos percetakan	F090058
+Air yang dibeli	F090006
+Tenaga elektrik yang dibeli	F090007
+Bahan pembakar, pelincir dan gas	F090008
+Kos barang(trading)	F090009
+Bayaran pembaikan dan penyelenggaraan semasa yang dibuat oleh pihak lain bagi harta tetap pertubuhan ini	F090011
+Perbelanjaan penyelidikan dan pembangunan	F090012
+%r&d : Sumber Dalaman/In-house	F090013
+%r&d : Sumber luaran/Outsource	F090014
+Bayaran pemprosesan dan lain-lain perkhidmatan yang berkaitan dengan teknologi maklumat	F090016
+Bayaran telekomunikasi (cth, telefon, internet dsb.)	F090017
+Bayaran faedah	F090025
+Pengiklanan dan promosi	F090063
+Bayaran bagi perkhidmatan keselamatan	F090067
+Bayaran perkhidmatan profesional lain (cth. bayaran perundingan arkitek, kejuruteraan, juruukur dsb.)	F090080
+Komisen dan bayaran agensi	F090109
+Pembelian perkhidmatan pengangkutan	F090119
+Perbelanjaan perjalanan(claim/parkir)	F091002
+Bayaran perakaunan, kesetiausahaan dan audit	F091003
+Bayaran guaman	F091004
+Bayaran pengurusan	F091005
+Perbelanjaan keraian	F091006
+Bayaran pos/kurier	F091007
+Bayaran bank	F091008
+Bayaran Sewa Tanah	F090019
+Bayaran Sewa Bangunan	F090020
+Bayaran Sewa Jentera dan kelengkapan	F090021
+Bayaran Sewa Kenderaan bermotor dan jenis pengangkutan lain	F090022
+Bayaran Sewa Lain-lain	F090023
+Susut nilai / perlunasan semasa ke atas harta tetap	F090024
+Bayaran royalti kpd:a) Kerajaan / Badan Berkanun (sila nyatakan jenis royalti)	F090026
+nota F090026	F090026a
+Bayaran royalti kpd:b)NGO / tajaan korporat (sila nyatakan jenis royalti)	F090027
+nota F090027	F090027a
+Jumlah bayaran yang diberi secara kontrak kepada pihak lain	F090059
+a) Taksiran (ke atas tanah dan bangunan) dan cukai tanah	F090030
+b) Cukai jalan	F090031
+c) Bayaran pendaftaran perniagaan, lesen memandu dsb.	F090032
+d) Cukai perkhidmatan atau cukai jualan	F091010
+Premium insurans kecuali insurans pampasan pekerja	F091009
+Belanja X Operasi:Kerugian daripada pertukaran wang asing/aset kewangan	F091011
+Belanja X Operasi:Kerugian daripada jualan/penilaian semula harta	F091012
+Belanja X Operasi:Hutang lapuk dihapuskan	F091013
+Belanja X Operasi:Pindahan semasa seperti pindahan wang, hadiah, derma, denda, dsb.	F091014
+Belanja X Operasi:Lain-lain perbelanjaan bukan operasi (sila nyatakan)	F091015
+nota F091015	F091015a
+Adakah perbelanjaan drpd aktiviti e-sukan?*	F091016
+Jika Ya, sila nyatakan jumlah perbelanjaan dalam aktiviti e-sukan ini?	F091017
+Lain-lain perbelanjaan operasi (sila nyatakan)	F090035
+nota F090035	F090035a
+Gaji & upah dibayar	F090036
+Bayaran pampasan, persaraan/pemberhentian kepada pekerja	F090037
+Bayaran manfaat:Rawatan perubatan percuma	F090038
+Bayaran manfaat:Lain-lain seperti makanan percuma, tempat tinggal percuma dsb.	F090039
+Caruman:Kumpulan Wang Simpanan Pekerja (KWSP/EPF)	F090040
+Caruman:Kumpulan wang simpanan lain	F090041
+Caruman:Pertubuhan Keselamatan Sosial (PERKESO/SOCSO)	F090042
+Caruman:Skim keselamatan sosial persendirian (cth: EIS/insurans pampasan pekerja dsb.)	F090043
+Caruman:Skim bayaran pampasan, persaraan/pemberhentian	F090044
+Bayaran kpd pengarah tidak bekerja kerana kehadiran mereka dlm mesyuarat Lembaga Pengarah	F090045
+Nilai pakaian percuma yg disediakan	F090046
+Kos latihan kepada pekerja	F090047
+Kos pengangkutan pekerja	F090048
+Bayaran levi pekerja	F090049
+Perbelanjaan pembayaran berasaskan saham kepada pekerja (termasuk saham & pilihan saham)	F090050
+Kos pekerja lain (sila nyatakan)	F090051
+nota F090051	F090051a
+Bayaran kepada pertubuhan lain yg membekalkan pekerja	F090052
+Jumlah perbelanjaan (9.1 hingga 9.36)	F090089
+Pindahan modal yang dibuat	F090053
+Perbelanjaan pajakan kewangan	F090054
+Dividen dibayar	F090055
+Cukai langsung dibayar (cth. cukai syarikat dan duti setem)	F090056
+JUMLAH BELANJA BESAR (9.37 hingga 9.41)	F090099
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
