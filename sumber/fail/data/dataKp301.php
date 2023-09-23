119bc0aaa49ba0e9760c9b6fc2dba6bd711df509
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-09-23 09:58:32 +0800

    tambah nota baru

--- sumber/fail/data/dataKp301.php
+++ sumber/fail/data/dataKp301.php
@@ -1,5 +1,22 @@
 <?php
 #--------------------------------------------------------------------------------------------------
+85102	Pendidikan pra-sekolah
+85104	Pendidikan rendah
+85212	Pendidikan menengah am
+85222	Pendidikan menengah teknik dan vokasional di bawah tahap pendidikan tinggi
+85302	Pendidikan kolej dan universiti
+85411	Pengajaran sukan
+85412	Pengajaran seni mempertahankan diri
+85419	Pendidikan sukan dan rekreasi lain yang t.t.t.l
+85421	Sekolah muzik dan tarian
+85429	Pendidikan kebudayaan lain yang t.t.t.l
+85491	Pusat tuisyen
+85492	Sekolah memandu
+85493	Pengajaran agama
+85494	Latihan komputer
+85499	Pendidikan lain t.t.t.l
+85500	Perkhidmatan sokongan pendidikan untuk peruntukan kepada perkhidmatan bukan pengajaran
+#--------------------------------------------------------------------------------------------------
 VALUE OF OUTPUT (F088101 + F088102 + F088107+ (F080007 - F090009) + F080011 +
 F080016 + F041399 +
 F080034 + F080035 + F080039 + F080061 + F080062 + F080013 +
@@ -14,10 +31,136 @@ F090020 + F090021 + F090022 + F090023 + F090027 +
 F090035 + F090045 + F090046 + F090047 + F090052 - (F100299 -  F100199))
 #--------------------------------------------------------------------------------------------------
 Cost of input DIVIDE by value of output must be between 0.2 to 0.7
+#--------------------------------------------------------------------------------------------------
+8.1
+Pendapatan daripada perkhimatan
+Income from services
+Yuran yang diterima oleh institusi pengajian tinggi dan sekolah (cth. yuran sekolah, yuran sukan dsb.)
+Perkhidmatan sokongan pendidikan iaitu perkhidmatan bukan pengajaran (cth. program pertukaran pelajar, perundingan pendidikan dsb.)
 
+Yuran yang diterima oleh institusi pengajian tinggi dan sekolah	F088101
+Perkhidmatan sokongan pendidikan iaitu perkhidmatan bukan pengajaran	F088102
+Nilai jualan daripada kerja perusahaan (cth. pakaian seragam sekolah dsb.)	F088107
+Nilai jualan (barang / bahan yang dibeli untuk dijual semula tanpa melalui proses selanjutnya)	F080007
+Pendapatan daripada perkhidmatan pengurusan	F080010
+Komisen dan brokeraj yang diterima	F080011
+Hasil Sewa Tanah	F080012
+Hasil Sewa Bangunan tempat kediaman	F080034
+Hasil Sewa Bangunan bukan tempat kediaman 	F080035
+Hasil Sewa Alat pengangkutan	F080039
+Hasil Sewa Jentera dan kelengkapan	F080061
+Hasil Sewa Perabot dan pemasangan	F080062
+Hasil Sewa Lain-lain(cth. lot kedai, ruang dsb.)	F080013
+Royalti, hakcipta, pelesenan dan yuran francais	F080014
+Jumlah Subsidi	F080063
+Subsidi gaji & upah	F080064
+Subsidi produk	F080065
+Subsidi pengeluaran	F080066
+Tuntutan & pampasan yg diterima	F080067
+Pemulihan hutang lapuk	F080068
+Pendapatan dari faedah	F080069
+Pendapatan dari dividen	F080070
+Keuntungan daripada jualan/penilaian semula harta	F080071
+Keuntungan daripada pertukaran mata wang asing/aset kewangan	F080072
+Kiriman wang, hadiah atau geran yg diterima	F080073
+Lain-lain pendapatan bukan operasi (sila nyatakan)	F080074
+nota Lain2 pendapatan bukan operasi	F080074a
+Adakah pertubuhan ini mempunyai pendapatan drp aktiviti e-sukan?	F080075
+Jika Ya, sila nyatakan jumlah pendapatan dalam aktiviti e-sukan ini	F080076
+Lain-lain pendapatan operasi (sila nyatakan)	F080016
+nota Lain2 pendapatan operasi	F080016a
+Jumlah pendapatan (8.1 hingga 8.10)	F080089
+Pindahan modal yg diterima	F080017
+JUMLAH HASIL BESAR (8.12 hingga 8.12)	F080099
 #--------------------------------------------------------------------------------------------------
+a) Bahan untuk pembaikan dan penyelenggaraan	F090003
+b) Alat tulis dan bekalan pejabat	F090005
+c) Lain-lain	F090057
+Kos percetakan	F090058
+Air yang dibeli	F090006
+Tenaga elektrik yang dibeli	F090007
+Bahan pembakar, pelincir dan gas	F090008
+Kos barang(trading)	F090009
+Makanan dan minuman (untuk pelajar)	F098101
+Bayaran pembaikan dan penyelenggaraan semasa yang dibuat oleh pihak lain bagi harta tetap pertubuhan ini	F090011
+Perbelanjaan penyelidikan dan pembangunan	F090012
+%r&d : Sumber Dalaman/In-house	F090013
+%r&d : Sumber luaran/Outsource	F090014
+Pembelian perkhidmatan pengangkutan	F090119
+Komisen dan bayaran agensi	F090109
+Perbelanjaan perjalanan(claim/parkir)	F091002
+Perbelanjaan keraian	F091006
+Bayaran perakaunan, kesetiausahaan dan audit	F091003
+Bayaran guaman	F091004
+Bayaran pengurusan	F091005
+Bayaran pos/kurier	F091007
+Bayaran bank	F091008
+Premium insurans kecuali insurans pampasan pekerja	F091009
+Bayaran perkhidmatan profesional lain (cth. bayaran perundingan arkitek, kejuruteraan, juruukur dsb.)	F090080
+Pengiklanan dan promosi	F090063
+Bayaran pemprosesan dan lain-lain perkhidmatan yang berkaitan dengan teknologi maklumat	F090016
+Bayaran telekomunikasi (cth, telefon, internet dsb.)	F090017
+Bayaran faedah	F090025
+Bayaran bagi perkhidmatan keselamatan	F090067
+Bayaran Sewa Tanah	F090019
+Bayaran Sewa Bangunan	F090020
+Bayaran Sewa Jentera dan kelengkapan	F090021
+Bayaran Sewa Kenderaan bermotor dan jenis pengangkutan lain	F090022
+Bayaran Sewa Lain-lain	F090023
+Susut nilai / perlunasan semasa ke atas harta tetap	F090024
+Bayaran royalti kpd:a) Kerajaan / Badan Berkanun (sila nyatakan jenis royalti)	F090026
+nota F090026	F090026a
+Bayaran royalti kpd:b)NGO / tajaan korporat (sila nyatakan jenis royalti)	F090027
+nota F090027	F090027a
+a) Taksiran (ke atas tanah dan bangunan) dan cukai tanah	F090030
+b) Cukai jalan	F090031
+c) Bayaran pendaftaran perniagaan, lesen memandu dsb.	F090032
+d) Cukai perkhidmatan atau cukai jualan	F091010
+Belanja X Operasi:Kerugian daripada pertukaran wang asing/aset kewangan	F091011
+Belanja X Operasi:Kerugian daripada jualan/penilaian semula harta	F091012
+Belanja X Operasi:Hutang lapuk dihapuskan	F091013
+Belanja X Operasi:Pindahan semasa seperti pindahan wang, hadiah, derma, denda, dsb.	F091014
+Belanja X Operasi:Lain-lain perbelanjaan bukan operasi (sila nyatakan)	F091015
+nota F091015	F091015a
+Adakah perbelanjaan drpd aktiviti e-sukan?*	F091016
+Jika Ya, sila nyatakan jumlah perbelanjaan dalam aktiviti e-sukan ini?	F091017
+Lain-lain perbelanjaan operasi (sila nyatakan)	F090035
+nota F090035	F090035a
+Gaji & upah dibayar	F090036
+Bayaran pampasan, persaraan/pemberhentian kepada pekerja	F090037
+Bayaran manfaat:Rawatan perubatan percuma	F090038
+Bayaran manfaat:Lain-lain seperti makanan percuma, tempat tinggal percuma dsb.	F090039
+Caruman:Kumpulan Wang Simpanan Pekerja (KWSP/EPF)	F090040
+Caruman:Kumpulan wang simpanan lain	F090041
+Caruman:Pertubuhan Keselamatan Sosial (PERKESO/SOCSO)	F090042
+Caruman:Skim keselamatan sosial persendirian (cth: EIS/insurans pampasan pekerja dsb.)	F090043
+Caruman:Skim bayaran pampasan, persaraan/pemberhentian	F090044
+Bayaran kpd pengarah tidak bekerja kerana kehadiran mereka dlm mesyuarat Lembaga Pengarah	F090045
+Nilai pakaian percuma yg disediakan	F090046
+Kos latihan kepada pekerja	F090047
+Kos pengangkutan pekerja	F090048
+Bayaran levi pekerja	F090049
+Perbelanjaan pembayaran berasaskan saham kepada pekerja (termasuk saham & pilihan saham)	F090050
+Kos pekerja lain (sila nyatakan)	F090051
+nota F090051	F090051a
+Bayaran kepada pertubuhan lain yg membekalkan pekerja	F090052
+Jumlah perbelanjaan (9.1 hingga 9.34)	F090089
+Pindahan modal yang dibuat	F090053
+Perbelanjaan pajakan kewangan	F090054
+Dividen dibayar	 F090055
+Cukai langsung dibayar (cth. cukai syarikat dan duti setem)	F090056
+JUMLAH BELANJA BESAR (9.37 hingga 9.39)	F090099
 #--------------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------
+Peringkat	Pendidikan
+Tadika	Kindergarden
+Sekolah rendah	Primary school
+Sekolah menengah rendah	Lower secondary school
+Sekolah menengah tinggi	Upper secondary school
+Sijil	Certificate
+Diploma
+Ijazah pertama	First degree
+Ijazah lanjutan	Postgraduate
+JUMLAH	TOTAL
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
